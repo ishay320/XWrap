23545826ff451cfe846b43b609e241c385038a8a
ishay320 <ishay320@hotmail.com>
2023-05-11 20:53:22 +0300

    first link and testing

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # XWrap: A Simple C Wrapper Renderer of X11
 
 XWrap is a lightweight and easy-to-use C library for rendering graphics on X11 window systems. It provides a simple and intuitive API for creating windows, drawing lines and shapes, and handling user input events.
+
+## Dependency
+
+```sh
+$ sudo apt install libx11-dev
+```
--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
+#include <X11/Xlib.h>
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
-    printf("hello world\n");
+    Display* display = XOpenDisplay(NULL);
+    if (display == NULL)
+    {
+        printf("Unable to connect X server\n");
+        return 1;
+    }
+
+    Window window = XCreateSimpleWindow(display, RootWindow(display, DefaultScreen(display)), 200,
+                                        0, 100, 100, 100, 0x000000, 0xFFFFFF);
+
+    XMapWindow(display, window);
+    XSync(display, False);
+
+    getchar();
     return 0;
 }
